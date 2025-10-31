44d90b7d022c6a924bede204bcbcb978ca778472
Jalal Jaleh <jalaljaleh@gmail.com>
2025-10-31 15:53:10 +0330

    -- remove symbol & currency enum types

--- Nobitex.Net/Entities/CurrencyType.cs
+++ Nobitex.Net/Entities/CurrencyType.cs
@@ -8,6 +8,5 @@ namespace Nobitex.Net
 {
     public enum CurrencyType
     {
-        rls, btc, eth, ltc, usdt, xrp, bch, bnb, eos, xlm, etc, trx, pmn, doge, uni, dai, link, dot, aave, ada, shib, ftm, matic, axs, mana, sand, avax, mkr, gmt, atom, uma, w, rsr, wld, 1m_nft, flow, agld, ton, mask, snt, agix, algo, ssv, band, omg, comp, zrx, rdnt, imx, 1inch, mdt, sushi, bico, gmx, zro, bal, dao, gal, not, nmr, xmr, enj, apt, lrc, dydx, grt, near, cvx, 100k_floki, fil, sol, ldo, crv, aevo, qnt, om, woo, storj, ant, 1m_btt, magic, ape, rndr, hbar, lpt, glm, blur, wbtc, meme, ethfi, egala, arb, fet, skl, cvc, snx, jst, ens, trb, chz, xtz, api3, slp, t, bat, 1b_babydoge, celr, yfi, egld, one, 1m_pepe, usdc
     }
 }
